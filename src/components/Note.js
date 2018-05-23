833d99b627adbdf356e388987a4a881d5792efdc
JD <jt7903@gmail.com>
2018-05-22 20:06:38 -0400

    one

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,32 @@
+import React, { Component } from 'react';
 import axios from 'axios';
-
+// import jsonp from 'jsonp';
 class Note extends Component {
+  
+
   getQuote() {
 
-    axios.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1')
+
+    // jsonp('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=', null, function (err, data) {
+    //   if (err) {
+    //     console.error(err.message);
+    //   } else {
+    //     console.log(data);
+    //   }
+    // });
+
+    axios.get('http://api.giphy.com/v1/gifs/random?api_key=3K2ZmyEMrXGGyR7EGBGnbti1HZNk2TZL')
       .then(res => {
         const note = res.data;
+        // this.setState({ });
 
-
+        console.log(note);
       });
   
+  }
+
+  componentDidMount() {
+    this.getQuote();
   }
 
   render() {
@@ -20,3 +37,5 @@ class Note extends Component {
     );
   }
 }
+
+export default Note;
